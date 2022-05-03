73e09b86eb4fa9de61d4237a0b332009e612af4f
Lin Pei Wen <mobilesuit0922@gmail.com>
2022-05-03 17:47:13 +0800

    132

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import LocalFetch from "./Pages/LocalFetch";
 import ContextLearn from "./Pages/Context";
 import Main from "./Pages/TodoList";
+import MainRedux from "./Pages/Redux";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class Game extends React.Component {
   constructor(props) {
@@ -17,6 +20,9 @@ class Game extends React.Component {
         <LocalFetch></LocalFetch>
         <ContextLearn></ContextLearn>
         <Main></Main>
+        <Provider store={store}>
+          <MainRedux></MainRedux>
+        </Provider>
       </Fragment>
     );
   }
--- src/reducer/todolist.js
+++ src/reducer/todolist.js
@@ -1,3 +1,4 @@
+import * as actions from "../Action/todolist";
 // 將資料結構列出以及預設初始資料。
 const initState = {
   todoList: ["first", "second"],
@@ -6,6 +7,11 @@ const initState = {
 // 初始資料、對該資料的動作
 const todoReducer = (state = initState, action) => {
   switch (action.type) {
+    case actions.ADD_TODO:
+      return {
+        ...state,
+        todoList: [...state.todoList, action.payload.todo],
+      };
     default:
       return state;
   }
